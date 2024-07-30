d4607dd311085206ed42811186cd3b39335e8c3b
SimonKling <simon.kling10@gmail.com>
2024-07-30 15:10:36 +0200

    fix

--- install.sh
+++ install.sh
@@ -30,19 +30,6 @@ sudo -u $USERNAME expect <<EOF
   spawn bash install.sh
   expect "Install? (y/n)" { send "y\r" }
   expect "password for $USERNAME:" {send "$PASSWORD\r"}
-  expect "Install Type? (h/s/m)" {send "h\r"}
-  expect "Enter your web user name:" {send "honeypot\r"}
-  expect "Enter password for your web user:" {send "$PASSWORD\r"}
-  expect "Repeat password you your web user:" {send "$PASSWORD\r"}
-  expect "Is this correct?" {send "y\r"}
-  expect "Install Type? (h/s/m)" {send "h\r"}
-  expect "Enter your web user name:" {send "honeypot\r"}
-  expect "Enter password for your web user:" {send "$PASSWORD\r"}
-  expect "Repeat password you your web user:" {send "$PASSWORD\r"}
-  expect "Is this correct?" {send "y\r"}
   expect eof
 EOF
 
-# Copy docker-compose.yml
-
-echo "T-Pot installation completed."
