7230d1b5acb0654882d5becd28c4903af6c63100
SimonKling <simon.kling10@gmail.com>
2024-07-30 14:28:56 +0200

    fix

--- installHoneypot.sh
+++ installHoneypot.sh
@@ -31,7 +31,7 @@ sudo -u $USERNAME expect <<EOF
   expect "Install? (y/n)" { send "y\r" }
   expect "password for $USERNAME:" {send "$PASSWORD\r"}
   expect "Install Type? (h/s/m)" {send "h\r"}
-  exspect "Enter your web user name:" {send "honeypot\r"}
+  expect "Enter your web user name:" {send "honeypot\r"}
   expect "Is this correct?" {send "y\r"}
   expect "Enter password for your web user:" {send "$PASSWORD\r"}
   expect "Repeat password you your web user:" {send "$PASSWORD\r"}
@@ -39,6 +39,6 @@ sudo -u $USERNAME expect <<EOF
 EOF
 
 # Copy docker-compose.yml
-sudo cp /path/to/docker-compose.yml /home/$USERNAME/tpotce/
+sudo cp docker-compose.yml /home/$USERNAME/tpotce/
 
 echo "T-Pot installation completed."
